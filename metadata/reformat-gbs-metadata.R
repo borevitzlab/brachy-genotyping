56e82da721384bf998c1a847a70c05a068574d7d
Kevin Murray <kdmfoss@gmail.com>
2017-05-26 16:04:38 +1000

    Add re-done keyfiles for bd 2-32

--- metadata/reformat-gbs-metadata.R
+++ metadata/reformat-gbs-metadata.R
@@ -1,16 +1,44 @@
 library(tidyverse)
 
-exp = read.delim("original-metadata/EXPERIMENT.tsv", stringsAsFactors=F)
-
-brachy_exp = exp %>%
-        filter(grepl("^brachy", plate.name, ignore.case=T)) %>%
-        filter(grepl("^pst", BARCODE, ignore.case=T)) %>%
-        filter(plate.name != "BrachyPelPlate1") %>%
-        select(plate.name, BARCODE, SequencerChemistry) %>%
-        mutate(plate.code = as.integer(sub('\\D*(\\d+)\\D*', '\\1', plate.name)),
+brachy_exp = read.csv("metadata/experiment.csv", stringsAsFactors=F) %>%
+        mutate(plate.code = as.integer(sub('\\D*(\\d+)\\D*', '\\1', sample.plate.name)),
                plate.code = sprintf("BR%02d", plate.code))
 
-# Rename dupicate XT-extracted plate
-brachy_exp$plate.code[brachy_exp$plate.code == "BR05" && brachy_exp$BARCODE == "PstIAdapterPlate6"] = "BR05XT"
+samp = read.csv("metadata/sampleplatelayout.csv", stringsAsFactors=F) %>%
+    filter(!is.na(plate.name)) %>% # remove the samples with missing metadata
+    mutate(plate.name = sub('^Brachypodium_Plate_', 'BrachypodiumPlate', plate.name)) %>%
+    filter(grepl("^brachy", plate.name, ignore.case=T)) %>%
+    rename(accession=value)
 
+# Copy of the brachy plate 5 for the XT samples
+samp.xt = filter(samp, plate.name=="BrachypodiumPlate5_PstI") %>%
+    mutate(plate.name="BrachypodiumPlate5_PstI_XT")
+samp = rbind(samp, samp.xt)
 
+barcodes = read.csv("metadata/barcode.csv", stringsAsFactors=F) %>%
+    separate(value, c("R1Barcode", "R2Barcode"), sep='/', fill='right') %>%
+    mutate(R1Barcode = sub(" ", "", R1Barcode),
+           R2Barcode = sub(" ", "", R2Barcode))
+
+brachy = samp %>%
+    left_join(brachy_exp, by=c("plate.name"="sample.plate.name")) %>%
+    filter(!is.na(plate.code)) %>% # remove plates not sequenced
+    left_join(barcodes, by=c("barcode.plate.name"="plate.name", "well"="well")) %>%
+    mutate(anon.name = paste0(plate.code, well))
+
+writeaxe = function(DF, prefix='keyfiles/') {
+    lane = unique(DF$lane)
+    if (all(DF$R2Barcode == "")) {
+        DF = DF[,c("R1Barcode", "anon.name")]
+    } else {
+        DF = DF[,c("R1Barcode", "R2Barcode", "anon.name")]
+    }
+    write.table(DF, paste0(prefix, lane,".axe"),
+                sep='\t', quote=F, row.names=F, col.names=F)
+    return(DF)
+}
+
+t = brachy %>%
+    select(lane, anon.name, R1Barcode, R2Barcode) %>%
+    group_by(lane) %>%
+    do(writeaxe(.))
